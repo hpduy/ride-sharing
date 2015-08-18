6c3f1a7102fcfc7e8ee0cbd3e59b421e66c8fc06
root <root@annn>
2015-08-18 13:31:59 +0700

    Create DAO

---
+++ src/main/java/com/bikiegang/ridesharing/dao/SocialTripAttendanceDao.java
@@ -0,0 +1,112 @@
+package com.bikiegang.ridesharing.dao;
+
+import com.bikiegang.ridesharing.annn.framework.common.LogUtil;
+import com.bikiegang.ridesharing.cache.RideSharingCA;
+import com.bikiegang.ridesharing.config.ConfigInfo;
+import com.bikiegang.ridesharing.database.Database;
+import com.bikiegang.ridesharing.pojo.SocialTripAttendance;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by hpduy17 on 6/26/15.
+ */
+public class SocialTripAttendanceDao {
+
+    Logger logger = LogUtil.getLogger(this.getClass());
+    private Database database = Database.getInstance();
+    RideSharingCA cache = RideSharingCA.getInstance(ConfigInfo.REDIS_SERVER);
+
+    public boolean insert(SocialTripAttendance obj) {
+        boolean result = false;
+        try {
+//            if (obj == null) {
+//                return false;
+//            }
+//            //put hashmap
+//            database.getAngelGroupHashMap().put(obj.getId(), obj);
+//            //Step 2: put redis
+//            result = cache.hset(obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj));
+//            if (result) {
+//                //Step 3: put job gearman
+//                short actionType = Const.RideSharing.ActionType.INSERT;
+//                JobEnt job = new JobEnt(0l, 0l, obj.getClass().getName(), actionType,
+//                        System.currentTimeMillis(), "", JSONUtil.Serialize(obj), "", "");
+//                result &= GClientManager.getInstance(ConfigInfo.RIDESHARING_WORKER_GEARMAN).push(job);
+//                if (!result) {
+//                    logger.error(String.format("Can't not insert DB with value=%s", obj));
+//                } else {
+//                    logger.info(String.format("Insert AngelGroup success with value=%s", JSONUtil.Serialize(obj)));
+//                }
+//            } else {
+//                logger.error(String.format("Can't not insert redis with key=%s, field=%s, value=%s",
+//                        obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj)));
+//            }
+        } catch (Exception ex) {
+            logger.error(ex.getStackTrace());
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean delete(SocialTripAttendance obj) {
+        boolean result = false;
+        try {
+//            //remove in hashmap
+//            database.getAngelGroupHashMap().remove(obj.getId());
+//            //Step 2: remove redis
+//            result = cache.hdel(obj.getClass().getName(), String.valueOf(obj.getId()));
+//
+//            if (result) {
+//                //Step 3: put job gearman
+//                short actionType = Const.RideSharing.ActionType.DELETE;
+//                JobEnt job = new JobEnt(0l, 0l, obj.getClass().getName(), actionType,
+//                        System.currentTimeMillis(), "", JSONUtil.Serialize(obj), "", "");
+//                result &= GClientManager.getInstance(ConfigInfo.RIDESHARING_WORKER_GEARMAN).push(job);
+//                if (!result) {
+//                    logger.error(String.format("Can't not delete DB with value=%s", obj));
+//                } else {
+//                    logger.info(String.format("Delete Angel success with value=%s", JSONUtil.Serialize(obj)));
+//                }
+//            } else {
+//                logger.error(String.format("Can't not delete redis with key=%s, field=%s, value=%s",
+//                        obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj)));
+//            }
+        } catch (Exception ex) {
+
+            logger.error(ex.getStackTrace());
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean update(SocialTripAttendance obj) {
+        boolean result = false;
+        try {
+//            //Update hashmap
+//            database.getAngelGroupHashMap().put(obj.getId(), obj);
+//            //Step 2: Update redis
+//            result = cache.hset(obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj));
+//            if (result) {
+//                //Step 3: put job gearman
+//                short actionType = Const.RideSharing.ActionType.UPDATE;
+//                JobEnt job = new JobEnt(0l, 0l, obj.getClass().getName(), actionType,
+//                        System.currentTimeMillis(), "", JSONUtil.Serialize(obj), "", "");
+//                result &= GClientManager.getInstance(ConfigInfo.RIDESHARING_WORKER_GEARMAN).push(job);
+//                if (!result) {
+//                    logger.error(String.format("Can't not update DB with value=%s", obj));
+//                } else {
+//                    logger.info(String.format("Update Angel with value=%s", JSONUtil.Serialize(obj)));
+//                }
+//            } else {
+//                logger.error(String.format("Can't not update redis with key=%s, field=%s, value=%s",
+//                        obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj)));
+//            }
+        } catch (Exception ex) {
+
+            logger.error(ex.getStackTrace());
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+}
