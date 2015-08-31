17b0cd25820527303a7902a118358e8c6e946fe6
root <root@annn>
2015-08-31 16:08:04 +0700

    Test

--- src/test/java/com/bikiegang/ridesharing/DAOTest/Insert.java
+++ src/test/java/com/bikiegang/ridesharing/DAOTest/Insert.java
@@ -6,8 +6,8 @@
 package com.bikiegang.ridesharing.DAOTest;
 
 import com.bikiegang.ridesharing.annn.framework.util.JSONUtil;
-import com.bikiegang.ridesharing.dao.TripCalendarDao;
-import com.bikiegang.ridesharing.pojo.TripCalendar;
+import com.bikiegang.ridesharing.dao.PlannedTripDao;
+import com.bikiegang.ridesharing.pojo.PlannedTrip;
 import com.bikiegang.ridesharing.utilities.TestUtils;
 
 /**
@@ -17,9 +17,9 @@ import com.bikiegang.ridesharing.utilities.TestUtils;
 public class Insert {
 
     public static void main(String[] args) {
-        TripCalendar _value = null;
-        _value = TestUtils.CreateTripCalendar();
-        TripCalendarDao dao = new TripCalendarDao();
+        PlannedTrip _value = null;
+        _value = TestUtils.CreatePlannedTrip();
+        PlannedTripDao dao = new PlannedTripDao();
         boolean DoAction = dao.insert(_value);
         System.out.println(DoAction);
         System.out.println(JSONUtil.Serialize(_value));
