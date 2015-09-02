14317c1ee4dc9958e338df04eaf58bc95785af6a
root <root@annn>
2015-09-02 11:50:25 +0700

    Test

--- src/main/java/com/bikiegang/ridesharing/geocoding/Pairing.java
+++ src/main/java/com/bikiegang/ridesharing/geocoding/Pairing.java
@@ -1,6 +1,5 @@
 package com.bikiegang.ridesharing.geocoding;
 
-
 import com.bikiegang.ridesharing.database.Database;
 import com.bikiegang.ridesharing.geocoding.GoogleRoutingObject.Bound;
 import com.bikiegang.ridesharing.pojo.LinkedLocation;
@@ -15,12 +14,13 @@ import java.util.*;
  * Created by hpduy17 on 6/23/15.
  */
 public class Pairing {
+
     private final double ACCEPTABLE_TIME = 24 * DateTimeUtil.HOURS;
     private final double REMOVEABLE_TIME = DateTimeUtil.DAYS;
     private Database database = Database.getInstance();
     /*
-    * MAIN ------------------------------------------------------------
-    * */
+     * MAIN ------------------------------------------------------------
+     * */
 
     // with the plannedTrip is exits
     public HashMap<Integer, List<PlannedTrip>> pair(PlannedTrip plannedTrip) throws Exception {
@@ -64,12 +64,13 @@ public class Pairing {
     }
 
     /*
-    * DRIVER ------------------------------------------------------------
-    * */
+     * DRIVER ------------------------------------------------------------
+     * */
     private List<PlannedTrip> getDriversCompatible(PlannedTrip plannedTrip) throws Exception {
-        List<Long> locationIds = database.getPlannedTripIdRFLinkedLocations().get(plannedTrip.getId());
-        if (null == locationIds || locationIds.size() < 2)
+        List<Long> locationIds = database.getPlannedTripIdRFLinkedLocations().get(plannedTrip.getRouteId());
+        if (null == locationIds || locationIds.size() < 2) {
             throw new Exception("List location is null or less than 2 location");
+        }
         LinkedLocation src = database.getLinkedLocationHashMap().get(locationIds.get(0));
         LinkedLocation des = database.getLinkedLocationHashMap().get(locationIds.get(locationIds.size() - 1));
         Route route = database.getRouteHashMap().get(plannedTrip.getRouteId());
@@ -80,7 +81,7 @@ public class Pairing {
         List<PlannedTrip> listDriverPlannedTripResult = new ArrayList<>();
         // you are passenger -> get start and end location of passenger
         GeoCell<Long> geoCell = database.getGeoCellDriver();
-        long passengerEpochday = plannedTrip.getDepartureTime()/ DateTimeUtil.SECONDS_PER_DAY;
+        long passengerEpochday = plannedTrip.getDepartureTime() / DateTimeUtil.SECONDS_PER_DAY;
         // get all plannedTrip id in cell and neighbor cell
         List<Long> srcNeighborList = new ArrayList<>(geoCell.getIdsInCellAndNeighbor(src.getLat(), src.getLng()));
         List<Long> desNeighborList = new ArrayList<>(geoCell.getIdsInCellAndNeighbor(des.getLat(), des.getLng()));
@@ -91,7 +92,6 @@ public class Pairing {
         desNeighborList.removeAll(srcNeighborList);
 
         //TODO START TO PARE
-
         for (long srcId : srcNeighborList) {
 
             // parse to long
@@ -122,8 +122,9 @@ public class Pairing {
                             //Get this plannedTrip
                             if (!listDriverPlannedTripResult.contains(driverPlannedTrip)) {
                                 // check helmet
-                                if (plannedTrip.isHasHelmet() || driverPlannedTrip.isHasHelmet())
+                                if (plannedTrip.isHasHelmet() || driverPlannedTrip.isHasHelmet()) {
                                     listDriverPlannedTripResult.add(driverPlannedTrip);
+                                }
                             }
                         } else {
                             //TODO: WARNING WHEN REMOVE HERE
@@ -143,17 +144,16 @@ public class Pairing {
     }
 
     /*
-    * PASSENGER ------------------------------------------------------------
-    * */
-
+     * PASSENGER ------------------------------------------------------------
+     * */
     /**
-     * Concept:
-     * 1/Get boundaries of driver plannedTrip -> get list passenger plannedTrip in this frame
-     * 2/Get src and des of each passenger plannedTrip -> get neighbor cell code of its
-     * 3/Check cell code is contain in list cell code of driver plannedTrip or not
-     * 4/If yes, check index of cell code near src and cell code near des in list cell code of driver to check direction
+     * Concept: 1/Get boundaries of driver plannedTrip -> get list passenger
+     * plannedTrip in this frame 2/Get src and des of each passenger plannedTrip
+     * -> get neighbor cell code of its 3/Check cell code is contain in list
+     * cell code of driver plannedTrip or not 4/If yes, check index of cell code
+     * near src and cell code near des in list cell code of driver to check
+     * direction
      */
-
     private List<PlannedTrip> getPassengersCompatible(PlannedTrip plannedTrip, Route route, List<LinkedLocation> linkedLocations) throws Exception {
         List<PlannedTrip> listPassengerPlannedTripResult = new ArrayList<>();
         long driverEpochday = plannedTrip.getDepartureTime() / DateTimeUtil.SECONDS_PER_DAY;
@@ -216,7 +216,6 @@ public class Pairing {
                                 if (driverPlannedTripCellCodes.contains(srcPassengerCellCode) && driverPlannedTripCellCodes.contains(desPassengerCellCode)) {
 
                                     // check direction , if srcIndex < desIndex in driver Planned Trip -> same direction
-
                                     if (driverPlannedTripCellCodes.indexOf(srcPassengerCellCode) < driverPlannedTripCellCodes.indexOf(desPassengerCellCode)) {
                                         long passengerGoTime = passengerPlannedTrip.getDepartureTime();
                                         long plannedTripGoTime = plannedTrip.getDepartureTime();
@@ -226,8 +225,9 @@ public class Pairing {
                                         // if same direction => check time
                                         if (DateTimeUtil.timeDistance(timeDriveReachNearSrcLocation, passengerGoTime) <= ACCEPTABLE_TIME) {
                                             if (!listPassengerPlannedTripResult.contains(passengerPlannedTrip)) {
-                                                if (plannedTrip.isHasHelmet() || passengerPlannedTrip.isHasHelmet())
+                                                if (plannedTrip.isHasHelmet() || passengerPlannedTrip.isHasHelmet()) {
                                                     listPassengerPlannedTripResult.add(passengerPlannedTrip);
+                                                }
                                             }
                                         } else {
                                             //TODO: WARNING WHEN REMOVE HERE
@@ -244,7 +244,6 @@ public class Pairing {
 
                 }
             }
-
 
         }
         return listPassengerPlannedTripResult;
