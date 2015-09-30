d6f3ce1c444103cf1e946655d06ba7f977f52ad8
root <root@server100>
2015-09-30 14:53:44 +0700

    feedback

--- src/main/java/com/bikiegang/ridesharing/dao/FeedbackDao.java
+++ src/main/java/com/bikiegang/ridesharing/dao/FeedbackDao.java
@@ -1,10 +1,15 @@
 package com.bikiegang.ridesharing.dao;
 
 import com.bikiegang.ridesharing.annn.framework.common.LogUtil;
+import com.bikiegang.ridesharing.annn.framework.gearman.GClientManager;
+import com.bikiegang.ridesharing.annn.framework.gearman.JobEnt;
+import com.bikiegang.ridesharing.annn.framework.util.JSONUtil;
 import com.bikiegang.ridesharing.cache.RideSharingCA;
 import com.bikiegang.ridesharing.config.ConfigInfo;
 import com.bikiegang.ridesharing.database.Database;
 import com.bikiegang.ridesharing.pojo.Feedback;
+import com.bikiegang.ridesharing.utilities.Const;
+import com.bikiegang.ridesharing.utilities.RequestLogger;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,7 +24,29 @@ public class FeedbackDao {
     public boolean insert(Feedback obj) {
         boolean result = false;
         try {
-
+            if (obj == null) {
+                return false;
+            }
+            RequestLogger.getInstance().info(obj, (short) Const.RideSharing.ActionType.INSERT);
+            //put hashmap
+            database.getFeedbackHashMap().put(obj.getId(), obj);
+            //Step 2: put redis
+            result = cache.hset(obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj));
+            if (result) {
+                //Step 3: put job gearman
+                short actionType = Const.RideSharing.ActionType.INSERT;
+                JobEnt job = new JobEnt(0l, 0l, obj.getClass().getName(), actionType,
+                        System.currentTimeMillis(), "", JSONUtil.Serialize(obj), "", "");
+                result &= GClientManager.getInstance(ConfigInfo.RIDESHARING_WORKER_GEARMAN).push(job);
+                if (!result) {
+                    logger.error(String.format("Can't not insert DB with value=%s", JSONUtil.Serialize(obj)));
+                } else {
+                    logger.info(String.format("Insert Feedback success with value=%s", JSONUtil.Serialize(obj)));
+                }
+            } else {
+                logger.error(String.format("Can't not insert redis with key=%s, field=%s, value=%s",
+                        obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj)));
+            }
         } catch (Exception ex) {
             logger.error(ex.getStackTrace());
             ex.printStackTrace();
@@ -30,7 +57,30 @@ public class FeedbackDao {
     public boolean delete(Feedback obj) {
         boolean result = false;
         try {
+            if (obj == null) {
+                return false;
+            }
+            RequestLogger.getInstance().info(obj, (short) Const.RideSharing.ActionType.DELETE);
+            //remove in hashmap
+            database.getFeedbackHashMap().remove(obj.getId());
+            //Step 2: remove redis
+            result = cache.hdel(obj.getClass().getName(), String.valueOf(obj.getId()));
 
+            if (result) {
+                //Step 3: put job gearman
+                short actionType = Const.RideSharing.ActionType.DELETE;
+                JobEnt job = new JobEnt(0l, 0l, obj.getClass().getName(), actionType,
+                        System.currentTimeMillis(), "", JSONUtil.Serialize(obj), "", "");
+                result &= GClientManager.getInstance(ConfigInfo.RIDESHARING_WORKER_GEARMAN).push(job);
+                if (!result) {
+                    logger.error(String.format("Can't not delete DB with value=%s", JSONUtil.Serialize(obj)));
+                } else {
+                    logger.info(String.format("Delete Feedback success with value=%s", JSONUtil.Serialize(obj)));
+                }
+            } else {
+                logger.error(String.format("Can't not delete redis with key=%s, field=%s, value=%s",
+                        obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj)));
+            }
         } catch (Exception ex) {
 
             logger.error(ex.getStackTrace());
@@ -42,8 +92,31 @@ public class FeedbackDao {
     public boolean update(Feedback obj) {
         boolean result = false;
         try {
-
+            if (obj == null) {
+                return false;
+            }
+            RequestLogger.getInstance().info(obj, (short) Const.RideSharing.ActionType.UPDATE);
+            //Update hashmap
+            database.getFeedbackHashMap().put(obj.getId(), obj);
+            //Step 2: Update redis
+            result = cache.hset(obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj));
+            if (result) {
+                //Step 3: put job gearman
+                short actionType = Const.RideSharing.ActionType.UPDATE;
+                JobEnt job = new JobEnt(0l, 0l, obj.getClass().getName(), actionType,
+                        System.currentTimeMillis(), "", JSONUtil.Serialize(obj), "", "");
+                result &= GClientManager.getInstance(ConfigInfo.RIDESHARING_WORKER_GEARMAN).push(job);
+                if (!result) {
+                    logger.error(String.format("Can't not update DB with value=%s", obj));
+                } else {
+                    logger.info(String.format("Update Feedback with value=%s", JSONUtil.Serialize(obj)));
+                }
+            } else {
+                logger.error(String.format("Can't not update redis with key=%s, field=%s, value=%s",
+                        obj.getClass().getName(), String.valueOf(obj.getId()), JSONUtil.Serialize(obj)));
+            }
         } catch (Exception ex) {
+
             logger.error(ex.getStackTrace());
             ex.printStackTrace();
         }
